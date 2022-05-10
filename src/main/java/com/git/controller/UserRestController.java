d81c0ee5274fecf3b97585dbd714545db9c60851
Taras Pikh <taraspih@gmail.com>
2022-05-10 18:30:11 +0300

    changes

--- src/main/java/com/git/controller/UserRestController.java
+++ src/main/java/com/git/controller/UserRestController.java
@@ -28,7 +28,9 @@ public class UserRestController {
     }
 
     public String deleteUser(@RequestBody Long userId) {
-        userService.deleteUser(userId);
-        return "removed";
+        if (userService.deleteUser(userId)) {
+            return "User removed";
+        };
+        return "Cannot delete user!";
     }
 }
